95fe36172223f9378c9e18705020df657c6e69fa
bybzmt <other@bybzmt.me>
2022-07-24 22:50:30 +0800

    save

--- webUI/src/pages/index.svelte
+++ webUI/src/pages/index.svelte
@@ -18,15 +18,48 @@
 </script>
 
 <Layout>
-    <div class="w-1/4">
-        <select rows={servers}>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Note</th>
+                <th>Addr</th>
+                <th colspan="3"></th>
+            </tr>
+        </thead>
+        <tbody>
             {#each servers as row}
-                <option value="{row}">{row.Note}</option>
+            <tr>
+                <td>{row.ID}</td>
+                <td>{row.Note}</td>
+                <td>{row.Addr}</td>
+                <td>
+                    <a href="#/logs?sid={row.ID}">Logs</a>
+                </td>
+                <td>
+                    <a href="#/logshow?sid={row.ID}">Edit</a>
+                </td>
+                <td>
+                    <a>Del</a>
+                </td>
+            </tr>
             {/each}
-        </select>
-        <button>logs</button>
-    </div>
+
+            <tr>
+                <td class="text-center" colspan="5">--</td>
+                <td>
+                    <a>Add</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </Layout>
 
 <style>
+    table{
+        margin: 1em;
+    }
+    td, th {
+        border: 1px solid #777;
+    }
 </style>
---
+++ webUI/src/pages/server_edit.svelte
@@ -0,0 +1,86 @@
+<script>
+    import Layout from "./lib/layout.svelte";
+    import { getContext, onMount, onDestroy } from "svelte";
+
+    let axios = getContext("axios");
+    let goto = getContext("goto");
+
+    let row = [];
+
+    function viewLogs() {
+        axios({ url: "/api/server" }).then((resp) => {
+            row = resp.data;
+        });
+    }
+
+    function save() {
+        let url = row.ID ? "/api/server/edit" : "/api/server/add";
+        axios({
+            method: "post",
+            url: url,
+            data: new URLSearchParams(row),
+        }).then(() => {
+            viewLogs();
+            row = {};
+        });
+    }
+
+    function cancel() {
+        goto("#/logs?sid="+row.ServerID)
+    }
+
+    onMount(() => {
+        viewLogs();
+    });
+</script>
+
+<Layout>
+        <div class="border">
+            <table>
+                <tr>
+                    <td>名称</td>
+                    <td><input class="border" bind:value={row.Note} /></td>
+                </tr>
+                <tr>
+                    <td>地址</td>
+                    <td><input class="border" bind:value={row.Addr} /></td>
+                </tr>
+                <tr>
+                    <td>用户名</td>
+                    <td><input class="border" bind:value={row.User} /></td>
+                </tr>
+                <tr>
+                    <td>验证方式</td>
+                    <td>
+                        <select class="border" bind:value={row.UsePwd}>
+                            <option value={false}>使用密钥</option>
+                            <option value={true}>使用密码</option>
+                        </select>
+                    </td>
+                </tr>
+                {#if row.UsePwd}
+                    <tr>
+                        <td>密码</td>
+                        <td><input class="border" bind:value={row.Passwd} /></td>
+                    </tr>
+                {:else}
+                    <tr>
+                        <td>密钥</td>
+                        <td><textarea class="border" bind:value={row.PrivateKey} /></td>
+                    </tr>
+                {/if}
+                <tr>
+                    <td colspan="2" class="text-center">
+                        {#if row.ID}
+                            <button on:click={save}>保存</button>
+                        {:else}
+                            <button on:click={cancel}>取消</button>
+                        {/if}
+                    </td>
+                </tr>
+            </table>
+        </div>
+</Layout>
+
+<style>
+</style>
